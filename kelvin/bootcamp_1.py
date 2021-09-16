d5855aa9fe92625aa724998fffabb1d81ebf3caa
Kelvin A. Melgar <kelvinmelgar2020@yahoo.com>
2021-09-15 17:43:09 -0700

    kelvin/bootcamp_1.py

---
+++ kelvin/bootcamp_1.py
@@ -0,0 +1,44 @@
+# print('Hi, this is your computer speaking')
+# print('I can run lots of prints of functionspy')
+
+# firstname = 'kelvin' 
+# print (firstname)
+# firstname = 'Alex'
+# print(firstname)
+# firstname = 'Walter'
+# print(firstname)
+# full_name = 'Kelvin Melgar'
+# print(full_name)
+
+print('1: Describe what is happening below by adding comments before each line')
+# creating a variable
+box_1 = 'books'
+# creating a variable
+box_2 = 'jacket'
+# creating a varible
+box_3 = 'plants'
+# creating  a variablels
+box_4 = 'food'
+
+print(box_1)
+box_1 = 'books'
+print(box_2)
+box_2 = 'jacket'
+print(box_3)
+box_3 = 'plants'
+print(box_4)
+box_4 = 'food'
+
+
+#print('3.1: Declare a variable with the name "address", assign it any street name you like')
+address = 'toland'
+print(address)
+# print('3.2: Print the address variable')
+
+# print('4: Reassign variables box_2 and box_4 with some other text and print box_1, box_2, box_3, box_4 again')
+
+# print('5.1: The line of code below is commented out because it produces many SyntaxErrors. Fix the problem and turn the comment back into regular Python code')
+completion_message = ('Completed the first Python challenge')
+print(completion_message)
+# print('5.2: Turn the comment below back into regular Python code')
+print(completion_message)
