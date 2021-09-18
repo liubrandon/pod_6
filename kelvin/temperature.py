137fcd2146a524684487bb885924e154c71d0f95
Kelvin A. Melgar <kelvinmelgar2020@yahoo.com>
2021-09-18 10:25:30 -0700

    kelvin/temperature.py

--- kelvin/temperature.py
+++ kelvin/temperature.py
@@ -1,3 +1,29 @@
-celsuis_100 = (100-32) * (5/9)
-print(celsuis_100)
+#Convert a temperature of 100 degrees fahrenheit to celsius
+
+
+
+celsius_100 = (100 - 32) * (5/9)
+print(celsius_100)
+print(type(celsius_100))
+
+#Convert a temperature of 0 degrees fahrenheit to celsius
+celsius_0 = (0 -32) * (5/9)
+print(celsius_0)
+print(type(celsius_0))
+
+#Convert a temperature of 34.2 degrees fahrenheit to celsius
+my_float1 = (34.2 - 32) * (5/9)
+print(my_float1)
+print(type(my_float1))
+
+#Convert a temperature of 34.2 degrees fahrenheit to celsius
+my_float2 = (34.2 - 32) * (5/9)
+print(my_float2)
+print(type(my_float2))
+
+# NOW,  CAN YOU CONVERT BACK?
+# Convert a temperature of 5 degrees celsius to fahrenheit
+my_float3 = ( 5 + 32) * (5/9)
+print(my_float3)
+print(type(my_float3))
 
