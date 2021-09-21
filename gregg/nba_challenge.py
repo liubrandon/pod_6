d912257be5d54cc7165303f8676ef060322f2548
Gregg Riess <businessman8994@gmail.com>
2021-09-20 21:03:58 -0400

    Shortened lines of code

--- gregg/nba_challenge.py
+++ gregg/nba_challenge.py
@@ -42,11 +42,28 @@ print(f'James Hardens three point percentage is {james_harden_3pt_percentage}')
 
 print('Challenge 3.1: Print out the paragraph but with only 1 sentence per line')
 # TODO: Print the giant chunk of text out using escape characters so each sentence comes out on a new line
-print('The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis. \nThey sent a package of Brandon Ingram, Lonzo Ball, Josh Hart, and 3 first-round picks to New Orleans to land Davis. \nThose three have made good developments with the Pelicans, especially Brandon Ingram. But, the deal is still a huge win for the Lakers as Lebron, Davis, and company have put together an incredible season. \nLos Angeles has ridden James and Davis, along with a supporting cast built around them, to the second-best record in the NBA. \nThe Lakers ended the season atop the Western Conference with a record of 49-14. \nThey were narrowly behind the Bucks for the best record in the league. \nDavis proved to the final piece necessary for the Lakers to rebound from missing the playoffís last year. \nLos Angeles was a dominant club on both sides of the ball and are in a position to have another successful year next season.')
+print('The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis. \
+\nThey sent a package of Brandon Ingram, Lonzo Ball, Josh Hart, and 3 first-round picks to New Orleans to land Davis.\
+\nThose three have made good developments with the Pelicans, especially Brandon Ingram. \
+But, the deal is still a huge win for the Lakers as Lebron, Davis, and company have put together an incredible season.\
+\nLos Angeles has ridden James and Davis, along with a supporting cast built around them, to the second-best record in the NBA. \
+\nThe Lakers ended the season atop the Western Conference with a record of 49-14. \
+\nThey were narrowly behind the Bucks for the best record in the league. \nDavis proved to the final piece necessary \
+for the Lakers to rebound from missing the playoffís last year. \nLos Angeles was a dominant club on both sides of the \
+ball and are in a position to have another successful year next season.')
 
 print('Challenge 3.2: Print out the paragraph but with only 1 sentence per line')
 # TODO: As above, print out the paragraph with only 1 sentence per line, and all in upper case
-print(('The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis. \nThey sent a package of Brandon Ingram, Lonzo Ball, Josh Hart, and 3 first-round picks to New Orleans to land Davis. \nThose three have made good developments with the Pelicans, especially Brandon Ingram. But, the deal is still a huge win for the Lakers as Lebron, Davis, and company have put together an incredible season. \nLos Angeles has ridden James and Davis, along with a supporting cast built around them, to the second-best record in the NBA. \nThe Lakers ended the season atop the Western Conference with a record of 49-14. \nThey were narrowly behind the Bucks for the best record in the league. \nDavis proved to the final piece necessary for the Lakers to rebound from missing the playoffís last year. \nLos Angeles was a dominant club on both sides of the ball and are in a position to have another successful year next season').upper())
+print(('The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis.\
+     \nThey sent a package of Brandon Ingram, Lonzo Ball, Josh Hart, and 3 first-round picks to New Orleans \
+to land Davis. \nThose three have made good developments with the Pelicans, especially Brandon Ingram. \
+But, the deal is still a huge win for the Lakers as Lebron, Davis, and company have put together an \
+incredible season. \nLos Angeles has ridden James and Davis, along with a supporting cast built around \
+them, to the second-best record in the NBA. \nThe Lakers ended the season atop the Western Conference \
+with a record of 49-14. \nThey were narrowly behind the Bucks for the best record in the league. \
+\nDavis proved to the final piece necessary for the Lakers to rebound from missing the playoffís \
+last year. \nLos Angeles was a dominant club on both sides of the ball and are in a position to \
+have another successful year next season').upper())
 
 print('Challenge 3.3: Make a boolean variable indicating whether you think the Lakers are the best NBA team')
 # TODO: make a variable called `lakers_are_best` to indicate this
