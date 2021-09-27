0cf05da98a5a2d90378e13a690208cbebe1163c3
rayaMooo <rayaannkelley@gmail.com>
2021-09-27 12:29:57 -0700

    NBA_Challenge_Raya

--- raya/nba_challenge.py
+++ raya/nba_challenge.py
@@ -1,7 +1,7 @@
 # print("Challenge 2.1:")
 jamal_murray_3pts_made = 46
 # TODO: Create variable here for number of 3 pt shots made by Fred VanVleet
-fred_vanVleet_3pts_made = 29
+fred_vanVleet_3pts_made = 43
 # TODO: Create variable here for number of 3 pt shots made by James Harden
 james_harden_3pts_made = 37
 
@@ -15,11 +15,11 @@ print(f'In the 2020 NBA playoffs, James Harden made {james_harden_3pts_made} 3 p
 
 print("Challenge 2.3: Store the number of three point shot attempts in variables for each player")
 # TODO: Create variable here for number of 3 pt shot attempts by Jamal Murray
-three_point_shot_attempts_jamal_murray = 45
+three_point_shot_attempts_jamal_murray = 93
 # TODO: Create variable here for number of 3 pt shot attempts by Fred VanVleet
-three_point_shot_attempts_fred_vanVleet = 40
+three_point_shot_attempts_fred_vanVleet = 110
 # TODO: Create variable here for number of 3 pt shot attempts by James Harden
-three_point_shot_attempts_james_harden = 42
+three_point_shot_attempts_james_harden = 109
 
 
 print("Challenge 2.4: Build on your print statement")
@@ -41,8 +41,8 @@ jh_average = print(f"The 3 point average for James Harden is {james_harden_3pts_
 
 print('Challenge 3.1: Print out the paragraph but with only 1 sentence per line')
 # TODO: Print the giant chunk of text out using escape characters so each sentence comes out on a new line
-
-print('Challenge 3.2: Print out the paragraph but with only 1 sentence per line')
+print("The Lakers went all in this offseason and swung a deal for former Pelicans forward Anthony Davis. \ They sent a package of Brandon Ingram, Lonzo Ball, Josh Hart, and 3 first-round picks to New Orleans to land Davis. \ Those three have made good developments with the Pelicans, especially Brandon Ingram. \ But, the deal is still a huge win for the Lakers as Lebron, Davis, and company have put together an incredible season. \ Los Angeles has ridden James and Davis, along with a supporting cast built around them, to the second-best record in the NBA. \ The Lakers ended the season atop the Western Conference with a record of 49-14. \ They were narrowly behind the Bucks for the best record in the league. \ Davis proved to the final piece necessary for the Lakers to rebound from missing the playoffís last year. \ Los Angeles was a dominant club on both sides of the ball and are in a position to have another successful year next season.")
+print("Challenge 3.2: Print out the paragraph but with only 1 sentence per line")
 # TODO: As above, orint out the paragraph with only 1 sentence per line, and all in upper case
 
 print('Challenge 3.3: Make a boolean variable indicating whether you think the Lakers are the best NBA team')
@@ -61,7 +61,10 @@ print(lakers_are_best)
 
 print('Challenge 3.5: Type Conversion Part 2')
 # TODO: Take each player's three point percentage (from part 2.5) and convert it to a string, then print it out.
-print(str(jh_average))
-print(str(fv_average))
-print(str(jm_average))
+jm_average = str(jamal_murray_3pts_made / three_point_shot_attempts_jamal_murray)
+jh_average = str(james_harden_3pts_made / three_point_shot_attempts_james_harden)
+fv_average = str(fred_vanVleet_3pts_made / three_point_shot_attempts_fred_vanVleet)
 # TODO: Take each player's three point percentage (from part 2.5) and convert it to an integer, then print it out.
+print(int(float(jm_average)))
+print(int(float(jh_average)))
+print(int(float(fv_average)))
