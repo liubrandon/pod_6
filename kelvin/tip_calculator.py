413b47cbef21a61fea74dd0fc2cfe6e1ba129b9c
Kelvin A. Melgar <kelvinmelgar2020@yahoo.com>
2021-10-04 13:51:24 -0700

    k/m calculator/challenge

---
+++ kelvin/tip_calculator.py
@@ -0,0 +1,23 @@
+  # WELCOME YOUR STEAKHOUSE # 
+
+
+               ###### # MENU######
+
+print(" Welcome, Hom many in your party?")
+number_of_people = input( "Welcome, How many in your party")
+
+Steak = 150
+New_York_Steak = 250
+
+
+
+
+
+Steak = input ("what would you be eating tonight?")
+print(Steak)
+
+price = input( "How many orders ?")
+print(price)
+
+tax = input(10 * {price})
+print(tax)
